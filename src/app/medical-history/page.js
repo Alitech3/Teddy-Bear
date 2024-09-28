b01afbc581f964b8b6ce5d08e731356594aba944
Aiden Letourneau <aidenletourneau@gmail.com>
2024-09-28 03:12:48 -0400

    sidebar in patient view is working

--- src/app/medical-history/page.js
+++ src/app/medical-history/page.js
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import Sidebar from "@/components/Sidebar"
 
 export default function Home() {
   return (
     <>
       <Sidebar/>
-      <h2>find a provider</h2>
+      <h2>Medical History</h2>
     </>
 
   );
---
+++ src/app/patient-view/page.js
@@ -0,0 +1,12 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function Home() {
+  return (
+    <>
+      <Sidebar/>
+      <h2>Patient View</h2>
+    </>
+      
+
+  );
+}
