11f2b91e0803fef947f046730ca129d628068ab1
Eric George <ericgeorge@Erics-MacBook-Air.local>
2024-09-28 05:38:24 -0400

    extra card component plus button

---
+++ src/app/login/page.js
@@ -0,0 +1,9 @@
+import { CardWithForm } from "@/components/ui/cardwithform";
+
+export default function Home() {
+  return (
+    <div className="flex flex-auto">
+    <CardWithForm></CardWithForm>
+    </div>
+    );
+}
