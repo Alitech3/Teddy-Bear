ee7284499fd7e3ad4150677ffe1bd532a9e7b702
Aiden Letourneau <aidenletourneau@gmail.com>
2024-09-28 14:45:59 -0400

    removed provider header

--- src/components/ProviderSidebar.jsx
+++ src/components/ProviderSidebar.jsx
@@ -6,17 +6,7 @@ const ProviderSidebar = () => {
     <div className="top-0 left-0 h-screen w-96 bg-Secondary text-white flex flex-col p-4 gap-4">
       <div>
         <Link href="/patient-view/my-medications">
-          <h3 className="hover:text-gray-400 cursor-pointer">My Medications</h3>
-        </Link>
-      </div>
-      <div>
-        <Link href="/patient-view/medical-history">
-          <h3 className="hover:text-gray-400 cursor-pointer">Medical History</h3>
-        </Link>
-      </div>
-      <div>
-        <Link href="/patient-view/find-a-provider">
-          <h3 className="hover:text-gray-400 cursor-pointer">Find a Provider</h3>
+          <h3 className="hover:text-gray-400 cursor-pointer">My Patients</h3>
         </Link>
       </div>
     </div>
