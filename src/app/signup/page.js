ec5eaf86586814856dd0fb42317d60a8706366b3
Ali C <18708388+Alitech3@users.noreply.github.com>
2024-09-28 15:28:41 -0400

    Centering signup page

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,14 +1,13 @@
 'use client';
-import { CardWithForm } from "@/components/ui/cardwithform";
+import { CardWithForm } from '@/components/ui/cardwithform';
 import { useRouter } from 'next/navigation';
 
 
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="grid grid-rows-3  justify-center">
-
-        <CardWithForm className = "justify-center" router={router}></CardWithForm>
+    <div className="flex h-full items-center justify-center">
+      <CardWithForm className = "justify-center" router={router}></CardWithForm>
     </div>
-    );
+  );
 }
