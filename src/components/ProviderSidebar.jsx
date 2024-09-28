3c7f26b9f5564a817fe1897b70138c1d01de9e8b
Aiden Letourneau <aidenletourneau@gmail.com>
2024-09-28 14:33:43 -0400

    patient sidebar and provider sidebar

---
+++ src/components/ProviderSidebar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from "next/link"
+
+const ProviderSidebar = () => {
+  return (
+    <div className="top-0 left-0 h-screen w-96 bg-Secondary text-white flex flex-col p-4 gap-4">
+      <div>
+        <Link href="/patient-view/my-medications">
+          <h3 className="hover:text-gray-400 cursor-pointer">My Medications</h3>
+        </Link>
+      </div>
+      <div>
+        <Link href="/patient-view/medical-history">
+          <h3 className="hover:text-gray-400 cursor-pointer">Medical History</h3>
+        </Link>
+      </div>
+      <div>
+        <Link href="/patient-view/find-a-provider">
+          <h3 className="hover:text-gray-400 cursor-pointer">Find a Provider</h3>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default ProviderSidebar
