36139ae5d6f2d17784a96275e0c97a6a0637c3e6
same.difference <dnmurillo@gmail.com>
2024-09-28 10:54:00 -0400

    renamed your medications to my medications

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,8 +5,8 @@ const Sidebar = () => {
   return (
     <div className="top-0 left-0 h-full w-64 bg-Secondary text-white flex flex-col p-4 gap-4">
       <div>
-        <Link href="/patient-view/your-medications">
-          <h3 className="hover:text-gray-400 cursor-pointer">Your Medications</h3>
+        <Link href="/patient-view/my-medications">
+          <h3 className="hover:text-gray-400 cursor-pointer">My Medications</h3>
         </Link>
       </div>
       <div>
