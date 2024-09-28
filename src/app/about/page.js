3bda2008ebe9f0a4472aeee46839f417c6bee924
Ali C <18708388+Alitech3@users.noreply.github.com>
2024-09-28 17:08:59 -0400

    about sucks

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -13,7 +13,7 @@ export default function Home() {
             width={100}
             height={100}/>
         </div>
-        <h1><b>Meddy</b> is the future.</h1>
+        <h1>Meddy is the future.</h1>
         <h2>A hypothetical one at least.</h2>
         <p>
             Meddy was created for <Link href="https://www.shellhacks.net/" target="_blank">ShellHacks 2024</Link> as a mock up for an all-in-one healthcare solution by:<br/> <Link href="https://github.com/Alitech3" target="_blank"><i>Ali Chapman</i></Link>, <Link href="https://github.com/myr124" target="_blank"><i>Eric George</i></Link>, <Link href="https://github.com/aidenletourneau" target="_blank"><i>Aiden Letourneau</i></Link>, & <Link href="https://github.com/same-difference" target="_blank"><i>Dianneth Murillo</i></Link>.
