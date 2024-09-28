154535e1f11ef164210383256b77b0c6aae19f56
Aiden Letourneau <aidenletourneau@gmail.com>
2024-09-28 19:38:36 -0400

    scaffolding for api routes

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const DATABASE_URL = "mongodb+srv://aidenletourneau:HTDz54m9ALGl9NkE@students.xtjkf.mongodb.net/?retryWrites=true&w=majority&appName=students";
+
+if (!DATABASE_URL) {
+  throw new Error("Please define the DATABASE_URL environment variable inside .env.local");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(DATABASE_URL, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default dbConnect;
