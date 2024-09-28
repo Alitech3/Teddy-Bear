1e067268c6d9d7ba304e299c62a2e3b4da553865
same.difference <dnmurillo@gmail.com>
2024-09-28 11:27:40 -0400

    Individualized medications page

--- src/app/patient-view/my-medications/page.js
+++ src/app/patient-view/my-medications/page.js
@@ -1,12 +1,24 @@
+import data from '../../../../data/patients.json';
+import prescriptionData from '../../../../data/prescriptions.json';
 import PatientLayout from "@/components/PatientLayout";
-import Sidebar from "@/components/Sidebar";
 
-export default function YourMedications() {
+export default function MyMedications({ patient_id }) {
+  const patient = data[100]
+  const myMeds = prescriptionData.filter(prescription => prescription.id === patient.medications)
   return (
 
       <>
           <PatientLayout>
-          <h2>Your Medications</h2>
+          <h2>My Medications</h2>
+          {myMeds.length > 0 ? (
+            <ul>
+              {Object.keys(myMeds[0]).filter((key, index) => index !== 0 && myMeds[0][key]).map((med, index) => (
+              <li key={index}>{myMeds[0][med]}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>No meds found</p>
+          )}
           </PatientLayout>
           
 
