09ce0323c483554169528f03634ea978c0af34e3
same.difference <dnmurillo@gmail.com>
2024-09-28 12:35:23 -0400

    refixed header and footer imports

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 
 import localFont from 'next/font/local';
 import './globals.css';
