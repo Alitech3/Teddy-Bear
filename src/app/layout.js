f4557d1b459163af63774ee6e25eb1ce94d8f5e1
Ali C <18708388+Alitech3@users.noreply.github.com>
2024-09-28 12:03:22 -0400

    strings and header component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,29 +1,30 @@
-import Footer from '../components/Footer';
+import Footer from '../components/footer';
+import Header from '../components/header';
 
-import localFont from "next/font/local";
-import "./globals.css";
-import Sidebar from "@/components/Sidebar";
+import localFont from 'next/font/local';
+import './globals.css';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata = {
-  title: "Teddy Bear",
-  description: "Your HealthCare provider companion",
+  title: 'Teddy Bear',
+  description: 'Your HealthCare provider companion',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header/>
         {children}
         <Footer/>
       </body>
