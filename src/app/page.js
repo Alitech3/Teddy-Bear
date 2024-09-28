7b7bd2a2e606fb63247f70773c9a4e61abd3da23
Ali C <18708388+Alitech3@users.noreply.github.com>
2024-09-28 12:02:52 -0400

    home header

--- src/app/page.js
+++ src/app/page.js
@@ -9,13 +9,12 @@ import Bear from '../../assets/home/Teddybear.png';
 // Sticky nav on scroll(?)
 
 export default function Home() {
-
-  
-
-
   return (
     <div>
       <div className="Nav">
+        <h1>
+          TeddyBear
+        </h1>
         <Link href={'./login'} id="Login">Login
         </Link>
         <Link href={'./signup'}id="Signup">Signup
@@ -23,7 +22,7 @@ export default function Home() {
       </div>
       <div className="Mission">
        <p>
-        Teddy Bear is on a mission to revolutionize the way healthcare is experienced by bridging the gap between communities, patients, and providers. Our platform fosters seamless collaboration, creating a cohesive support system that empowers patients while strengthening the relationships between care teams and the broader community. By integrating cutting-edge technology with a human-centered approach, Teddy Bear aims to deliver personalized, accessible care and foster a sense of connection that goes beyond traditional healthcare models
+        TeddyBear is on a mission to revolutionize the way healthcare is experienced by bridging the gap between communities, patients, and providers. Our platform fosters seamless collaboration, creating a cohesive support system that empowers patients while strengthening the relationships between care teams and the broader community. By integrating cutting-edge technology with a human-centered approach, Teddy Bear aims to deliver personalized, accessible care and foster a sense of connection that goes beyond traditional healthcare models
        </p>
        <Image
         src={Bear}
