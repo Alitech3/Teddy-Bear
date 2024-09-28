3a154f7cc967f2302b2621216dce03f5d1c82b40
same.difference <dnmurillo@gmail.com>
2024-09-28 12:12:34 -0400

    updated med history page again or sm I don't remember anymore

--- src/app/patient-view/medical-history/page.js
+++ src/app/patient-view/medical-history/page.js
@@ -9,6 +9,81 @@ export default function Home() {
     <>
       <PatientLayout>
       <h2>Medical History</h2>
+      <p>{patient.first_name} {patient.last_name}</p>
+      <img src={patient.image} />
+      <table>
+        <tr>
+          <td>
+            DOB
+          </td>
+          <td>
+          {patient.date_of_birth}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Sex
+          </td>
+          <td>
+          {patient.sex}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Height
+          </td>
+          <td>
+          {patient.height}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Weight
+          </td>
+          <td>
+          {patient.weight}
+          </td>
+        </tr>
+      </table>
+
+      <h3>Contact Info</h3>
+      <table>
+        <tr>
+          <td>
+            Phone Number
+          </td>
+          <td>
+          {patient.phone_number}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Email
+          </td>
+          <td>
+          {patient.email}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Home Address
+          </td>
+          <td>
+          {patient.home_address}
+          </td>
+        </tr>
+      </table>
+
+      <h3>Diagnoses</h3>
+      <p>{patient.diagnoses}</p>
+
+      <h3>Allergies</h3>
+      <p>{patient.allergies}</p>
+
+      <h3>Treatment Notes</h3>
+      <p>{patient.treat_notes}</p>
+      
+      <h3>Recent Visits</h3>
       {myVisits.length > 0 ? (
           <table>
             <th>
