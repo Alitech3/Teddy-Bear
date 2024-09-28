ee38d91a432bc200f7043685d6a7c055447b1878
same.difference <dnmurillo@gmail.com>
2024-09-28 14:30:45 -0400

    formatted 404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,17 @@
-import '../css/404.css';
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 
 export default function NotFound() {
 	return(
-		<p>
-			404 Not Found
+		<div className="flex flex-col w-full h-full justify-center items-center">
+		<h1 className="flex text-9xl pb-20">
+			404 Error
+		</h1>
+		<p className="flex text-3xl pb-7">
+			Looks like someone made a fucky-wucky :c
 		</p>
+		<Button asChild><Link href="/">Go Back Home</Link></Button>
+		</div>
 	);
 }
