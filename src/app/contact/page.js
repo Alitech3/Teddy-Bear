9a291556e0a7464a1a0354ea636ac729e1221450
same.difference <dnmurillo@gmail.com>
2024-09-29 02:05:40 -0400

    contact page prettified

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,44 +1,69 @@
-import Footer from '@/components/Footer';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import PageTitle from "@/components/PageTitle";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Footer from "@/components/Footer";
 
 export default function Contact() {
   return (
     <>
-      <div className='flex h-screen justify-center'>
-        <h1>Phone Directory</h1>
-        <table>
-          <tr>
-            <td>Medical Emergency</td>
-            <td>911</td>
-          </tr>
-          <tr>
-            <td>Suicide and Crisis Line</td>
-            <td>988</td>
-          </tr>
-          <tr>
-            <td>Poison Control Center</td>
-            <td>1-800-222-1222</td>
-          </tr>
-          <tr>
-            <td>Substance Abuse and Mental Health Services Helpline</td>
-            <td>1-800-662-4357</td>
-          </tr>
-          <tr>
-            <td>Center For Disease Control and Prevention</td>
-            <td>1-800-232-4636</td>
-          </tr>
-          <tr>
-            <td>Medicare Helpline</td>
-            <td>1-800-633-4227</td>
-          </tr>
-          <tr>
-            <td>Health Insurance Marketplace</td>
-            <td>1-800-318-2596</td>
-          </tr>
-          <tr>
-            <td>Planned Parenthood</td>
-            <td>1-800-230-7526</td>
-          </tr>
-        </table>
+      <div className="flex h-screen place-items-center justify-center">
+        <Card className="h-fit pb-6">
+          <CardHeader>
+            <CardTitle>Phone Directory</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableRow>
+                <TableHead>Medical Emergency</TableHead>
+                <TableCell>911</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHead>Suicide and Crisis Line</TableHead>
+                <TableCell>988</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHead>Poison Control Center</TableHead>
+                <TableCell>1-800-222-1222</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHead>
+                  Substance Abuse and Mental Health Services Helpline
+                </TableHead>
+                <TableCell>1-800-662-4357</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHead>Center For Disease Control and Prevention</TableHead>
+                <TableCell>1-800-232-4636</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHead>Medicare Helpline</TableHead>
+                <TableCell>1-800-633-4227</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHead>Health Insurance Marketplace</TableHead>
+                <TableCell>1-800-318-2596</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableHead>Planned Parenthood</TableHead>
+                <TableCell>1-800-230-7526</TableCell>
+              </TableRow>
+            </Table>
+          </CardContent>
+        </Card>
       </div>
       <Footer />
     </>
