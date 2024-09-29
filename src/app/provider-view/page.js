0c348e549508d62676197c1e9f31bc8e8402715b
Eric George <ericgeorge@Erics-MacBook-Air.local>
2024-09-29 04:30:46 -0400

    made providerlist tile

--- src/app/provider-view/page.js
+++ src/app/provider-view/page.js
@@ -1,14 +1,14 @@
-import ProviderSidebar from '@/components/ProviderSidebar';
-import data from '../../../data/patients.json';
-import PatientCard from '@/components/PatientCard';
-import ProviderLayout from '@/components/ProviderLayout';
-import Link from 'next/link';
+import ProviderSidebar from "@/components/ProviderSidebar";
+import data from "../../../data/patients.json";
+import PatientCard from "@/components/PatientCard";
+import ProviderLayout from "@/components/ProviderLayout";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <ProviderLayout>
-        <div className='flex flex-col gap-10 pt-5'>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 h-screen">
           {data.map(
             (d, index) =>
               index <= 10 && (
