a8438f0e9a08886c731f24794bf6103e998de72e
Eric George <ericgeorge@Erics-MacBook-Air.local>
2024-09-28 20:40:51 -0400

    changes to prescribe

--- src/app/provider-view/my-patients/[id]/page.js
+++ src/app/provider-view/my-patients/[id]/page.js
@@ -8,6 +8,9 @@ import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Label } from '@/components/ui/label';
 import { DataTableDemo } from '@/components/Datatable';
 import { Accordion, AccordionItem, AccordionTrigger, AccordionContent} from '@/components/ui/accordion';
+import ProviderLayout from '@/components/ProviderLayout';
+import data from '../../../../../data/patients.json';
+
 
 const openai = new OpenAI({
   apiKey: '',
@@ -35,33 +38,36 @@ const Page = ({pname="Test"}) => {
   };
 
   return (
-    <div className='p-6'>
-    <Label className=" p-6 text-white text-2xl">Meddy.ai</Label>
-    <Input
-            id="i1"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            placeholder="Hello, what is your name?"
-          />
-          <Button onClick={clickHandler} className="bg-blue-600 text-white hover:bg-blue-700">
-            Submit
-          </Button>
-          <div className="text-white text-lg font-semibold">{text}</div>
-       <Label className=" p-6 text-white text-2xl">{pname}</Label>
-        <DataTableDemo className="gap-4"></DataTableDemo>
-        <DataTableDemo className="p-6"></DataTableDemo>
-      
-        <div className='flex flex-col items-center justify-center space-y-4 p-6'>
-          {visible && (
-            <Alert onClose={resetAlert} className="transition-opacity duration-300">
-              <Terminal className="h-4 w-4" />
-              <AlertTitle>Heads up!</AlertTitle>
-              <AlertDescription>This is a valid combination</AlertDescription>
-            </Alert>
-          )}
+    <>
+        <ProviderLayout>
+        <div className='flex flex-col gap-10 pt-5'>
+          <Label className=" p-6 text-white text-2xl">{pname}</Label>
+            <DataTableDemo className="gap-4"></DataTableDemo>
+            <DataTableDemo className="p-6"></DataTableDemo>
           
+            <div className='flex flex-col items-center justify-center space-y-4 p-6'>
+              {visible && (
+                <Alert onClose={resetAlert} className="transition-opacity duration-300">
+                  <Terminal className="h-4 w-4" />
+                  <AlertTitle>Heads up!</AlertTitle>
+                  <AlertDescription>This is a valid combination</AlertDescription>
+                </Alert>
+              )}
+              <Label className=" p-6 text-white text-2xl">Meddy.ai</Label>
+              <Input
+                id="i1"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                placeholder="Hello, what is your name?"
+              />
+              <Button onClick={clickHandler} className="bg-blue-600 text-white hover:bg-blue-700">
+                Submit
+              </Button>
+              <div className="text-white text-lg font-semibold">{text}</div>
+            </div>
         </div>
-    </div>
+        </ProviderLayout>
+    </>
   );
 };
 
--- src/app/provider-view/my-patients/page.js
+++ src/app/provider-view/my-patients/page.js
@@ -1,17 +1,21 @@
-import data from '../../../../data/patients.json';
-import PatientCard from '@/components/PatientCard';
-import ProviderLayout from '@/components/ProviderLayout';
+import data from "../../../../data/patients.json";
+import PatientCard from "@/components/PatientCard";
+import ProviderLayout from "@/components/ProviderLayout";
+import Link from "next/link";
 
 export default function MyPatients() {
-  return(
-    <>
-      <ProviderLayout>
-        <div className="flex flex-col gap-10 pt-5">
-          {data.map((d, index) => ( index <= 10 &&
+  return (
+    <ProviderLayout>
+      <div className="flex flex-col gap-10 pt-5">
+        {data.map(
+          (d, index) =>
+            index <= 10 && (
+              <Link key={index} href={`/provider-view/my-patients/${index}`}>
                 <PatientCard key={index} patient={d} />
-          ))}
-        </div>
-      </ProviderLayout>
-    </>
+              </Link>
+            )
+        )}
+      </div>
+    </ProviderLayout>
   );
 }
