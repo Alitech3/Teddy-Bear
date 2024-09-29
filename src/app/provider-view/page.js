b523102a5e1f6aa55c539aa8b50757b07cc1e590
Ali C <18708388+Alitech3@users.noreply.github.com>
2024-09-29 04:37:47 -0400

    unused vars

--- src/app/provider-view/page.js
+++ src/app/provider-view/page.js
@@ -1,21 +1,10 @@
 'use client';
-import ProviderSidebar from '@/components/ProviderSidebar';
 import data from '../../../data/patients.json';
 import PatientCard from '@/components/PatientCard';
 import ProviderLayout from '@/components/ProviderLayout';
 import Link from 'next/link';
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [dat, setData] = useState();
-
-  console.log('hey');
-
-  useEffect(() => {
-    fetch('https://meddybear.co/api/patients').then((e) => console.log(e));
-  })
-
-
   return (
     <>
       <ProviderLayout>
