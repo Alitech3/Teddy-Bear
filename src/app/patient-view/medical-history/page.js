469709e7f9e057e2ff6d5a66d1f5313ee2635d46
same.difference <dnmurillo@gmail.com>
2024-09-29 01:18:15 -0400

    medical-history is now pretty

--- src/app/patient-view/medical-history/page.js
+++ src/app/patient-view/medical-history/page.js
@@ -10,6 +10,16 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import PageTitle from "@/components/PageTitle";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const PatientLayout = dynamic(() => import("@/components/PatientLayout"), {
   ssr: false,
@@ -22,85 +32,121 @@ export default function Home() {
   return (
     <Suspense fallback={<>Loading...</>}>
       <PatientLayout>
-        <Card>
-          <CardHeader>
-            <CardTitle>Medical History</CardTitle>
-          </CardHeader>
-          <CardContent className="flex flex-row">
-            <div className="">
-              <img src={patient.image} className="w-[200px]" />
-              <h2>
+        <div className="grid grid-cols-2 gap-6 max-w-fit mx-auto">
+          <div className="col-span-2">
+            <PageTitle text={"Medical History"} />
+          </div>
+          <Card className="col-span-1">
+            <CardHeader>
+              <CardTitle>
                 {patient.first_name} {patient.last_name}
-              </h2>
-              <table>
-                <tr>
-                  <td>DOB</td>
-                  <td>{patient.date_of_birth}</td>
-                </tr>
-                <tr>
-                  <td>Sex</td>
-                  <td>{patient.sex}</td>
-                </tr>
-                <tr>
-                  <td>Height</td>
-                  <td>{patient.height}</td>
-                </tr>
-                <tr>
-                  <td>Weight</td>
-                  <td>{patient.weight}</td>
-                </tr>
-              </table>
-            </div>
-            <div>
-              <h3>Contact Info</h3>
-              <table>
-                <tr>
-                  <td>Phone Number</td>
-                  <td>{patient.phone_number}</td>
-                </tr>
-                <tr>
-                  <td>Email</td>
-                  <td>{patient.email}</td>
-                </tr>
-                <tr>
-                  <td>Home Address</td>
-                  <td>{patient.home_address}</td>
-                </tr>
-              </table>
-            </div>
-          </CardContent>
-        </Card>
-
-        <h3>Diagnoses</h3>
-        <p>{patient.diagnoses}</p>
-
-        <h3>Allergies</h3>
-        <p>{patient.allergies}</p>
-
-        <h3>Treatment Notes</h3>
-        <p>{patient.treat_notes}</p>
-
-        <h3>Recent Visits</h3>
-        {myVisits.length > 0 ? (
-          <table>
-            <th>
-              <td>Date</td>
-              <td>Provider</td>
-              <td>Reason</td>
-            </th>
-            {myVisits.map((visit, index) => (
-              <tr key={index}>
-                <td>{visit.date}</td>
-                <td>
-                  {visit.provider_first_name} {visit.provider_last_name}
-                </td>
-                <td>{visit.reason}</td>
-              </tr>
-            ))}
-          </table>
-        ) : (
-          <p>Make your first appointment today!</p>
-        )}
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-row space-x-6">
+              <div>
+                <img src={patient.image} className="w-[200px]" />
+              </div>
+              <div>
+                <Table>
+                  <TableRow>
+                    <TableHead>DOB</TableHead>
+                    <TableCell>{patient.date_of_birth}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableHead>Sex</TableHead>
+                    <TableCell>{patient.sex}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableHead>Height</TableHead>
+                    <TableCell>{patient.height}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableHead>Weight</TableHead>
+                    <TableCell>{patient.weight}</TableCell>
+                  </TableRow>
+                </Table>
+              </div>
+            </CardContent>
+          </Card>
+          <Card className="col-span-1">
+            <CardHeader>
+              <CardTitle>Contact Info</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Table>
+                <TableRow>
+                  <TableHead>Phone Number</TableHead>
+                  <TableCell>{patient.phone_number}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHead>Email</TableHead>
+                  <TableCell>{patient.email}</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableHead>Home Address</TableHead>
+                  <TableCell>{patient.home_address}</TableCell>
+                </TableRow>
+              </Table>
+            </CardContent>
+          </Card>
+          <Card className="col-span-1">
+            <CardHeader>
+              <CardTitle>Diagnoses</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <ul>
+                {patient.diagnoses.map((diagnosis, index) => (
+                  <li className="pb-2 ml-2" key={index}>
+                    {diagnosis}
+                  </li>
+                ))}
+              </ul>
+            </CardContent>
+          </Card>
+          <Card className="col-span-1">
+            <CardHeader>
+              <CardTitle>Allergies</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <ul>
+                {patient.allergies.map((allergy, index) => (
+                  <li className="pb-2 ml-2" key={index}>
+                    {allergy}
+                  </li>
+                ))}
+              </ul>
+            </CardContent>
+          </Card>
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle>Recent Visits</CardTitle>
+            </CardHeader>
+            <CardContent>
+              {myVisits.length > 0 ? (
+                <Table>
+                  <TableHeader>
+                    <TableHead>Date</TableHead>
+                    <TableHead>Provider</TableHead>
+                    <TableHead>Reason</TableHead>
+                  </TableHeader>
+                  {myVisits.map((visit, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{visit.date}</TableCell>
+                      <TableCell>
+                        {visit.provider_first_name} {visit.provider_last_name}
+                      </TableCell>
+                      <TableCell>{visit.reason}</TableCell>
+                    </TableRow>
+                  ))}
+                </Table>
+              ) : (
+                <CardDescription>
+                  Make your first appointment today!
+                </CardDescription>
+              )}
+            </CardContent>
+          </Card>
+        </div>
       </PatientLayout>
     </Suspense>
   );
