ea8a4cb6dfe4eb77ffaa5ea0a748d5bf0b6bb55b
Ali C <18708388+Alitech3@users.noreply.github.com>
2024-09-29 02:55:23 -0400

    flattened providers

--- src/components/ProviderSidebar.jsx
+++ src/components/ProviderSidebar.jsx
@@ -5,14 +5,14 @@ import { Mail } from "lucide-react";
 
 const ProviderSidebar = () => {
   return (
-    <div className='top-0 left-0 h-screen w-52 bg-Secondary text-white flex flex-col p-4 gap-4 font-bold pt-20'>
+    <div className="top-0 left-0 h-screen w-52 bg-Secondary text-white flex flex-col p-4 gap-4 font-bold pt-20">
       <div>
-        <Link href='/provider-view/my-patients'>
-          <h3 className='hover:text-gray-400 cursor-pointer'>My Patients</h3>
+        <Link href="/provider-view">
+          <h3 className="hover:text-gray-400 cursor-pointer">My Patients</h3>
         </Link>
       </div>
-      <div className='flex flex-col h-full justify-end'>
-        <Mail className='flex-' />
+      <div className="flex flex-col h-full justify-end">
+        <Mail className="flex-" />
       </div>
     </div>
   );
