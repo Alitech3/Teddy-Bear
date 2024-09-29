3fec399edcb07b3ffac4a0acda6f07285a2bf726
Ali C <18708388+Alitech3@users.noreply.github.com>
2024-09-29 02:42:50 -0400

    provider view flattened

--- src/app/provider-view/page.js
+++ src/app/provider-view/page.js
@@ -1,9 +1,24 @@
-import ProviderSidebar from "@/components/ProviderSidebar";
+import ProviderSidebar from '@/components/ProviderSidebar';
+import data from '../../../data/patients.json';
+import PatientCard from '@/components/PatientCard';
+import ProviderLayout from '@/components/ProviderLayout';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <ProviderSidebar />
+      <ProviderLayout>
+        <div className='flex flex-col gap-10 pt-5'>
+          {data.map(
+            (d, index) =>
+              index <= 10 && (
+                <Link key={d.id} href={`/provider-view/my-patients/${d.id}`}>
+                  <PatientCard patient={d} />
+                </Link>
+              )
+          )}
+        </div>
+      </ProviderLayout>
     </>
   );
 }
